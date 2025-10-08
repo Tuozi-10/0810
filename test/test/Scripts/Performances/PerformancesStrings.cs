10560a94043d84246b1c3d99cfdb165d17025017
gab.nrs <villeroy.gab@gmail.com>
2025-10-08 15:01:27 +0200

    ton commit en bas

--- test/test/Scripts/Performances/PerformancesStrings.cs
+++ test/test/Scripts/Performances/PerformancesStrings.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 
 namespace test.Scripts;
 
@@ -14,16 +15,25 @@ public class PerformancesStrings
 
     private void CheckPerfsLotOfStrings()
     {
+        var sw1 = new Stopwatch();
+        sw1.Start();
+        
         string myString = "";
 
         for (int i = 0; i < 100; i++)
         {
             myString += i.ToString();
         }
+        
+        sw1.Stop();
+        Console.WriteLine($"method 1 exe time = {sw1.Elapsed.TotalMilliseconds} seconds");
     }
     
     private void CheckPerfsStringBuilder()
     {
+        var sw2 = new Stopwatch();
+        sw2.Start();
+        
         StringBuilder stringBuilder = new StringBuilder();
         
         for (int i = 0; i < 100; i++)
@@ -32,6 +42,9 @@ public class PerformancesStrings
         }
 
         string myString = stringBuilder.ToString();
+        
+        sw2.Stop();
+        Console.WriteLine($"method 2 exe time = {sw2.Elapsed.TotalMilliseconds} seconds");
     }
 
     private void CheckPerfsPrimeNumbersList()
