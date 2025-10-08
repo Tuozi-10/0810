f56be85ea0e6b304726cb27f4dc1684ac8d38301
Hippolyte <jombarthippolyte@gmail.com>
2025-10-08 14:23:25 +0200

    Push midi
    Exercice sur les strings et sur le tri

--- test/test/Scripts/Performances/PerformancesSort.cs
+++ test/test/Scripts/Performances/PerformancesSort.cs
@@ -1,4 +1,4 @@
-﻿namespace test.Scripts;
+namespace test.Scripts;
 
 using System.Diagnostics;
 
@@ -9,6 +9,7 @@ public class PerformancesSort
         CheckStandardSort();
         CheckCustomSort();
         CheckLinqSort();
+        CheckCustomSortArray();
     }
 
     private List<int> GenerateRandomList(int count)
@@ -32,38 +33,83 @@ public class PerformancesSort
         sw.Start();
 
         var list = GenerateRandomList(10000);
-
+        
+        list.Sort();
         // TODO SORT
         
         sw.Stop();
         Console.WriteLine("Sort Standard Elapsed={0}",sw.Elapsed);
     }
+    
+    private void CheckCustomSortArray()
+    {
+        var sw = new Stopwatch();
+        var sw2 = new Stopwatch();
 
+        var list = GenerateRandomList(1000);
+        int[] intArray = new int[list.Count];
+        
+        for (int i = 0; i < list.Count; i++)
+        {
+            intArray[i] = list[i];
+        }
+        
+        sw.Start();
+        for (int i = 0; i < list.Count-1; i++)
+        {
+            if (list[i] > list[i + 1])
+            {
+                (list[i], list[i+1]) = (list[i+1], list[i]);
+                i = -1;
+            }
+        }
+        sw.Stop();
+        
+        sw2.Start();
+        for (int i = 0; i < intArray.Length-1; i++)
+        {
+            if (intArray[i] > intArray[i + 1])
+            {
+                (intArray[i], intArray[i+1]) = (intArray[i+1], intArray[i]);
+                i = -1;
+            }
+        }
+        sw2.Stop();
+        
+        Console.WriteLine("Sort Custom Elapsed for list = {0}",sw.Elapsed);
+        Console.WriteLine("Sort Custom Elapsed for array = {0}",sw2.Elapsed);
+    }
+    
     private void CheckCustomSort()
     {
         var sw = new Stopwatch();
         sw.Start();
 
-        var list = GenerateRandomList(10000);
-        int temp;
-
-        //EXEMPLE INVERSION, CA SORT PAS TOUT
-        for (int i = 0; i < list.Count; i++)
+        var list = GenerateRandomList(100);
+        var list2 = new List<int>(list);
+        
+        for (int i = 0; i < list.Count-1; i++)
         {
             if (list[i] > list[i + 1])
             {
-                temp = list[i + 1];
-                list[i + 1] = list[i];
-                list[i] = temp;
+                (list[i], list[i+1]) = (list[i+1], list[i]);
+                i = -1;
             }
         }
-        
-        
         
         // todo sort
         
         sw.Stop();
-        Console.WriteLine($"Sort Linq");
+        list2.Sort();
+        for (int i = 0; i < list2.Count; i++)
+        {
+            if (list[i] != list2[i])
+            {
+                Console.WriteLine("Ca marche pas");
+                return;
+            }
+        }
+        Console.WriteLine("Sort Custom Elapsed = {0}",sw.Elapsed);
     }
     
     private void CheckLinqSort()
@@ -72,13 +118,11 @@ public class PerformancesSort
         sw.Start();
 
         var list = GenerateRandomList(10000);
-
-        //  list.OrderBy()
-        
+        var list2 = list.OrderBy(x => x);
         // todo sort
         
         sw.Stop();
-        Console.WriteLine($"Sort Linq");
+        Console.WriteLine("Sort Linq Elapsed = {0}",sw.Elapsed);
     }
     
     // TODO CHECK WITH ARRAYS ALL THE SAME METHODS 
--- test/test/Scripts/Performances/PerformancesStrings.cs
+++ test/test/Scripts/Performances/PerformancesStrings.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Diagnostics;
 
 namespace test.Scripts;
 
@@ -14,24 +15,34 @@ public class PerformancesStrings
 
     private void CheckPerfsLotOfStrings()
     {
+        var sw = new Stopwatch();
+        sw.Start();
         string myString = "";
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 10000; i++)
         {
             myString += i.ToString();
         }
+        
+        sw.Stop();
+        Console.WriteLine("Check perf lot of strings = {0}",sw.Elapsed);
     }
     
     private void CheckPerfsStringBuilder()
     {
+        var sw = new Stopwatch();
+        sw.Start();
         StringBuilder stringBuilder = new StringBuilder();
         
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 10000; i++)
         {
             stringBuilder.Append(i);
         }
 
         string myString = stringBuilder.ToString();
+        
+        sw.Stop();
+        Console.WriteLine("Check perf string builder  = {0}",sw.Elapsed);
     }
 
     private void CheckPerfsPrimeNumbersList()
