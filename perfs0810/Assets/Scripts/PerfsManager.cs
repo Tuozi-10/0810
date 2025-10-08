5fcaf86076ab9c5178658646ecdebbc3ebf8d660
maxenced-dev <maxence.desceliers@gmail.com>
2025-10-08 17:03:10 +0200

    Bidule

---
+++ perfs0810/Assets/Scripts/PerfsManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+
+public class PerfsManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(DoChecks());
+    }
+    
+    private IEnumerator DoChecks()
+    {
+        yield return new WaitForSeconds(2f);
+        CheckTruc1();
+        CheckTruc2();
+        CheckTruc3();
+        CheckTruc4();
+        CheckTruc5();
+        CheckTruc6();
+    }
+
+    private void CheckTruc1()
+    {
+        Profiler.BeginSample("test 1");
+        string t = "";
+        for (int i = 0; i < 50000; i++)
+        {
+            t += i.ToString();
+        }
+
+        Profiler.EndSample();
+    }
+
+    private void CheckTruc2()
+    {
+        Profiler.BeginSample("test 2");
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        for (int i = 0; i < 50000; i++)
+        {
+            stringBuilder.Append(i);
+        }
+
+        string myString = stringBuilder.ToString();
+        Profiler.EndSample();
+    }
+    
+    private void CheckTruc3()
+    {
+        Profiler.BeginSample("test 3");
+        string t = "";
+        for (int i = 0; i < 1; i++)
+        {
+            t += i.ToString();
+        }
+
+        Profiler.EndSample();
+    }
+
+    private void CheckTruc4()
+    {
+        Profiler.BeginSample("test 4");
+        StringBuilder stringBuilder = new StringBuilder();
+        
+        for (int i = 0; i < 1; i++)
+        {
+            stringBuilder.Append(i);
+        }
+
+        string myString = stringBuilder.ToString();
+        Profiler.EndSample();
+    }
+    
+    
+    private List<int> GenerateRandomList(int count)
+    {       
+        var randomList = new List<int>();
+        
+        for (int i = 0; i < count; i++)
+        {
+            int value = Random.Range(0,int.MaxValue);
+            randomList.Add(value);
+        }
+
+        return randomList;
+    }
+    
+    private void CheckTruc5()
+    {
+        Profiler.BeginSample("test 5");
+        var list = GenerateRandomList(100000);
+        list.Sort();
+        Profiler.EndSample();
+    }
+    
+    private void CheckTruc6()
+    {
+        Profiler.BeginSample("test 6");
+        
+        var list = GenerateRandomList(100000);
+        var list2 = list.OrderBy(x=>x);
+        
+        Profiler.EndSample();
+    }
+}
+
